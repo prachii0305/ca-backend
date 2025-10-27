ff2bc35f4d33cc4af44f8dd3f8dec5fd3f8a5723
prachii0305 <114798139+prachii0305@users.noreply.github.com>
2025-10-28 00:06:58 +0530

    hg

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://ca-frontend-p1b2rttb1-prachi-gandhis-projects.vercel.app'],
+  origin: ['http://localhost:3000', 'https://ca-frontend-fk69co33w-prachi-gandhis-projects.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
