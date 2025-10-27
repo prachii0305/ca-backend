1a7b1cd90cda627c658eb17a7993c923a503c35d
prachii0305 <114798139+prachii0305@users.noreply.github.com>
2025-10-27 23:55:51 +0530

    gg

--- api/index.js
+++ api/index.js
@@ -1,5 +1,3 @@
 const app = require('../server'); // Import the Express app
 
-module.exports = (req, res) => {
-  return app(req, res); // Handle requests with Vercel's serverless function
-};
+module.exports = app;
